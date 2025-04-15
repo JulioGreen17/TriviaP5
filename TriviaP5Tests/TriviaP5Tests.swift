eed808d5b63e109575f9c9bc46f567b392db0cd1
Julio <juliovar123@gmail.com>
2025-04-15 16:32:30 -0700

    Initial Commit

---
+++ TriviaP5Tests/TriviaP5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaP5Tests.swift
+//  TriviaP5Tests
+//
+//  Created by Julio Varela on 4/15/25.
+//
+
+import Testing
+@testable import TriviaP5
+
+struct TriviaP5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
